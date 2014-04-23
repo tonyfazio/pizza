8cabd27cad867afedb632e35f14e22330b4a2613
Tony Fazio <antoniojfazio@gmail.com>
2014-04-23 14:46:31 -0500

    used let in spec file

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -5,16 +5,18 @@ describe Pizza do
   it "exists" do
     expect(Pizza).to be_a(Class)
   end
+
   describe '.initialize' do
     it 'records all of the toppings' do
       toppings = [
         Topping.new("mushrooms", vegetarian: true),
         Topping.new("pepperoni")
       ]
-      pizza =Pizza.new(toppings)
+      pizza = Pizza.new(toppings)
 
       expect(pizza.toppings).to eq(toppings)
     end
+
     it 'defaults the toppings to cheese only, if the pizza has no toppings' do
       pizza = Pizza.new
 
@@ -28,16 +30,21 @@ describe Topping do
   it "exists" do
     expect(Topping).to be_a(Class)
   end
+
   describe '.initialize' do
+
+    let(:topping) { Topping.new('olives') }
     it "sets the name of the topping" do
-      topping = Topping.new('olives')
+      # topping = Topping.new('olives')
 
       expect(topping.name).to eq('olives')
     end
-    it "sets whether or not the topping is vegetarian" do
-      topping = Topping.new("bell peppers", vegetarian: true)
 
-      expect(topping.vegetarian).to eq(true)
+    let(:vegetarian_topping) { Topping.new("bell peppers", vegetarian: true) }
+    it "sets whether or not the topping is vegetarian" do
+      # topping = Topping.new("bell peppers", vegetarian: true)
+
+      expect(vegetarian_topping.vegetarian).to eq(true)
     end
   end
 end
